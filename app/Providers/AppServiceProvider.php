357acfa826fe1f510227019e616cec9d22ab3f09
Luiz Felipe <lepinholima@gmail.com>
2026-04-16 15:57:51 -0300

    feat: configure AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\{DB, Http, URL};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->bootModels();
+        $this->bootUrl();
+        $this->bootTests();
+        $this->bootCommands();
+    }
+
+    private function bootModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict(
+            !$this->app->isProduction()
+        );
+    }
+
+    private function bootUrl(): void
+    {
+        URL::forceHttps($this->app->isProduction());
+    }
+
+    private function bootTests(): void
+    {
+        Http::preventingStrayRequests(!$this->app->isProduction());
+    }
+
+    private function bootCommands(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->isProduction());
     }
 }
